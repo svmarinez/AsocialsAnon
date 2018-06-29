7fd110bd09c5b5be7690a9cecbcac14db0e559a0
Sarai Vasquez <svmarinez@hotmail.com>
2018-06-29 12:13:52 +0200

    almost almost

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 window.onload = function() {
     document.getElementById("start-button").onclick = function() {
         window.location.href = "game.html";
-        window.onload = function() {
-            var game = new Game("canvas");
-            game.startGame();
-}}}
+             
+    }
+}
